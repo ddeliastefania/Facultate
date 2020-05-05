c90bba749e371d1f0a81d970a692abb223ddd16b
Delia Dedeaga <Delia.Dedeaga@kantar.com>
2020-05-05 21:06:48 +0300

    Lab 11 Asp. NetCore Razor Pages

---
+++ Asp_Dedeaga_Delia_Rp/Asp_Dedeaga_Delia_Rp/RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie.Data
+{
+    public class RazorPagesMovieContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public RazorPagesMovieContext(
+            DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<RazorPagesMovie.Models.Movie> Movie
+        {
+            get; set;
+        }
+    }
+}
---
+++ Asp_Dedeaga_Delia_Rp/Asp_Dedeaga_Delia_Rp/RazorPagesMovie/Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+
+namespace RazorPagesMovie
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        #region snippet_ConfigureServices
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            services.AddDbContext<RazorPagesMovieContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("RazorPagesMovieContext")));
+        }
+        #endregion
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
